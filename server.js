e4c8878cf4d34530c557bd498f2cd47ad20e95ce
fand <fand.gmork@gmail.com>
2017-10-07 15:16:43 +0900

    Remove unused code

--- server.js
+++ server.js
@@ -3,17 +3,7 @@
 // const path = require('path');
 const http = require('http');
 const express = require('express');
-// const webpack = require('webpack');
-// const webpackDevMiddleware = require('webpack-dev-middleware')
-// const webpackConfig = require('./webpack.config');
-
-// const compiler = webpack(webpackConfig);
-
-// Create the app, setup the webpack middleware
 const app = express();
-// app.use(webpackDevMiddleware(compiler, {
-//   publicPath: webpackConfig.output.path,
-// }));
 
 app.use(express.static('./client'));
 
@@ -22,14 +12,7 @@ const io = require('socket.io')(server);
 
 const PORT = process.argv[2] || 3000;
 
-console.log(PORT);
-// console.log(webpackConfig.output.path);
-
-// server.listen(PORT);
-
 io.on('connection', socket => {
-  console.log('a user connected');
-
   socket.on('ready', () => {
     socket.broadcast.emit('ready');
   });
@@ -54,5 +37,5 @@ io.on('connection', socket => {
 });
 
 server.listen(PORT, () => {
-  console.log('listening on *:', PORT);
+  console.log(`[glsl-livecoder] Server launched on http://localhost:${PORT}/`);
 });
