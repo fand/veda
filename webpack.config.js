75133b4cf51e821e2353c96c818efd85ee51022f
fand <fand.gmork@gmail.com>
2017-10-17 13:36:33 +0900

    Move server/client codes to lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './client/index.js'),
+  entry: path.resolve(__dirname, './lib/client.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './client/'),
