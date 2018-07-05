abd33886dad887145b3a8f4dd269fc4878f3d635
Jonathan Giroux <giroux.jo@gmail.com>
2018-07-05 15:16:06 +0200

    Add example for glslify-import

---
+++ examples/common.glsl
@@ -0,0 +1,7 @@
+// This file goes with *glslify.frag*,
+// although all examples could benefit from a common prolog,
+// if only the linter would support glslify.
+
+precision mediump float;
+uniform float time;
+uniform vec2 resolution;
