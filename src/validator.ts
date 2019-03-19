aaf7addac95ef2f5878651b2a9ac4a19fca9d1e0
fand <fand.gmork@gmail.com>
2019-03-20 00:29:43 +0900

    Rewrite validator with async/await

--- src/validator.ts
+++ src/validator.ts
@@ -3,31 +3,33 @@ import * as execa from 'execa';
 import * as tmp from 'tmp';
 import * as p from 'pify';
 
-export function validator(
+export async function validator(
     glslangValidatorPath: string,
     shader: string,
     postfix: string,
 ): Promise<void> {
-    let tmpfile: string = '';
-    return p(tmp.file)({ keep: true, postfix, discardDescriptor: true })
-        .then(path => {
-            tmpfile = path;
-            return p(writeFile)(tmpfile, shader, 'utf8');
-        })
-        .then(() => execa(glslangValidatorPath, [tmpfile]))
-        .then(result => {
-            if (result.stdout.match(/ERROR/)) {
-                throw new Error(result.stdout);
-            }
-        });
+    const tmpfile = await p(tmp.file)({
+        keep: true,
+        postfix,
+        discardDescriptor: true,
+    });
+
+    await p(writeFile)(tmpfile, shader, 'utf8');
+
+    const result = await execa(glslangValidatorPath, [tmpfile]);
+    if (result.stdout.match(/ERROR/)) {
+        throw new Error(result.stdout);
+    }
 }
 
-export function loadFile(glslangValidatorPath: string, filePath: string) {
-    return execa(glslangValidatorPath, [filePath])
-        .then(result => {
-            if (result.stdout.match(/ERROR/)) {
-                throw new Error(result.stdout);
-            }
-        })
-        .then(() => p(readFile)(filePath, 'utf8'));
+export async function loadFile(
+    glslangValidatorPath: string,
+    filePath: string,
+): Promise<string> {
+    const result = await execa(glslangValidatorPath, [filePath]);
+    if (result.stdout.match(/ERROR/)) {
+        throw new Error(result.stdout);
+    }
+
+    return await p(readFile)(filePath, 'utf8');
 }
